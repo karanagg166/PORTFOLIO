d8fcb00028afe3c6c238407d48c1752d7d74e841
karanagg166 <22bcs130.@iiitdmj.ac.in>
2024-10-09 05:29:10 +0530

    build completed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Button } from "react-bootstrap";
 import Particle from "../Particle";
-import pdf from "../../Assets/Priyanshu's CV.pdf";
+import pdf from "../../Assets/karan_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
