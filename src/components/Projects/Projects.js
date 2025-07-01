f0e30499ec8e513bdaec47d31e4da4f2e092590c
karanagg166 <22bcs130.@iiitdmj.ac.in>
2025-07-01 14:39:41 +0530

    changes made

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,10 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Quick-Clinic"
+              description="A smart healthcare platform enabling real-time chat and video consultations between patients and doctors. Features include appointment booking, medical history tracking, doctor availability, and secure digital interactions."
+              ghLink="https://github.com/karanagg166/quickclinic"
+              demoLink="https://quickclinic-49jb4atli-karan-aggarwals-projects.vercel.app/"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Penny Saver"
+              description="A personal finance tracker with interactive date-based analytics. Users can log daily transactions, view financial trends through graphs, categorize expenses, and generate reports—making budgeting smarter and more efficient."
+              ghLink="https://github.com/karanagg166/PennySaver"
+              demoLink="https://penny-saver-r26w9jga7-karan-aggarwals-projects.vercel.app/"
             />
           </Col>
 
@@ -47,10 +47,12 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Task-Flow"
+              description="A smart task management app designed for productivity. Features include task creation, deadline tracking, status updates, and category filters — all within a clean, user-friendly interface to streamline daily workflows.
+    
+"
+              ghLink="https://github.com/karanagg166/TaskFlow"
+              demoLink="https://task-flow-qu2dg91bw-karan-aggarwals-projects.vercel.app/"              
             />
           </Col>
 
@@ -58,35 +60,23 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Shop Sizzle"
+              description="A scalable e-commerce platform built with TypeScript. Features include advanced product search and filters, secure checkout, real-time order tracking, and user account management — optimized for high-volume transactions and seamless shopping."
+              ghLink="https://github.com/karanagg166/ShopSizzle"
+              demoLink="https://github.com/karanagg166/ShopSizzle"
             />
           </Col>
-
-          <Col md={4} className="project-card">
+           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={leaf}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Stock-Pilot"
+              description="A real-time stock tracking and portfolio management tool. Users can monitor live prices, analyze trends, and manage their investments efficiently — all through an intuitive dashboard designed for informed trading decisions."
+              ghLink="https://github.com/karanagg166/stockpilot"
+              demoLink="https://github.com/karanagg166/stockpilot"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+         
         </Row>
       </Container>
     </Container>
